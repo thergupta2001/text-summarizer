8b89d73d0cafe7dbde5efc8eebee9dde7f9e34ee
thergupta2001 <2001.rohangupta@gmail.com>
2024-02-02 16:59:36 +0530

    testing mode

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,25 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db.js')
+// const dotenv = require('dotenv').config()
+// const connectDB = require('./config/db.js')
 
-const authRoutes = require('./routes/authRoutes.js');
-const openaiRoutes = require('./routes/openaiRoutes.js')
-const errorHandler = require('./middlewares/errorMiddleware.js');
+// const authRoutes = require('./routes/authRoutes.js');
+// const openaiRoutes = require('./routes/openaiRoutes.js')
+// const errorHandler = require('./middlewares/errorMiddleware.js');
 
 const app = express();
 
-connectDB();
+// connectDB();
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
-app.use(morgan('dev'))
-app.use(errorHandler)
+// app.use(morgan('dev'))
+// app.use(errorHandler)
 
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/openai', openaiRoutes)
+// app.use('/api/v1/auth', authRoutes)
+// app.use('/api/v1/openai', openaiRoutes)
 
 app.get('/test',(req,res)=>{
      return res.status(200).json({
@@ -28,6 +28,6 @@ app.get('/test',(req,res)=>{
      })
 })
 
-app.listen(process.env.PORT, () => {
-     console.log(`Server on port ${process.env.PORT}`)
+app.listen(8080, () => {
+     console.log(`Server on port 8080`)
 })
