d7b4dadee6edb270ed245dcfbc80b0c889922bbb
thergupta2001 <2001.rohangupta@gmail.com>
2024-02-02 18:11:10 +0530

    backend testing for deployment

--- backend/routes/openaiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const { main, completions } = require('../controllers/openaiController')
-
-router.post('/summary', main)
-router.post('/completion', completions)
-
-module.exports = router
