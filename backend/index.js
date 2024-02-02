26f7b6bec76a9550aaef0f10aafebf14f98aa6b9
thergupta2001 <2001.rohangupta@gmail.com>
2024-02-02 18:18:12 +0530

    backend testing for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,6 @@
-const bodyParser = require('body-parser');
 const express = require('express')
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv').config()
 
 const app = express();
-
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.json())
-app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
      return res.status(200).json({
