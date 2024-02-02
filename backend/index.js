8320d3e3b78930aebb5cdfda9eaf4a45fef68d36
thergupta2001 <2001.rohangupta@gmail.com>
2024-02-02 17:03:29 +0530

    testing mode

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
-// const dotenv = require('dotenv').config()
-// const connectDB = require('./config/db.js')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db.js')
 
-// const authRoutes = require('./routes/authRoutes.js');
-// const openaiRoutes = require('./routes/openaiRoutes.js')
-// const errorHandler = require('./middlewares/errorMiddleware.js');
+const authRoutes = require('./routes/authRoutes.js');
+const openaiRoutes = require('./routes/openaiRoutes.js')
+const errorHandler = require('./middlewares/errorMiddleware.js');
 
 const app = express();
 
@@ -16,8 +16,8 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
-// app.use(morgan('dev'))
-// app.use(errorHandler)
+app.use(morgan('dev'))
+app.use(errorHandler)
 
 // app.use('/api/v1/auth', authRoutes)
 // app.use('/api/v1/openai', openaiRoutes)
