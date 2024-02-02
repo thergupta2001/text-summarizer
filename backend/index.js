ae746f66924c2adaf18d4986d4fbf476c15922b8
thergupta2001 <2001.rohangupta@gmail.com>
2024-02-02 18:15:04 +0530

    backend testing for deployment

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/test',(req,res)=>{
+app.get('/',(req,res)=>{
      return res.status(200).json({
           message : "hi",
      })
